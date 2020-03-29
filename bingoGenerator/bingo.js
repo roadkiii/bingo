4a109398c72f6fc6d4d5c32f151caa7923c8f083
Robin Lundin <robin.lundin@protonmail.com>
2020-03-29 11:36:15 +0200

    css fix

--- bingoGenerator/bingo.js
+++ bingoGenerator/bingo.js
@@ -1,21 +1,71 @@
 var allNumbers = [];
 
+var drawn = 0;
+
+var chosenNrRef = document.getElementById('number');
+var drawnNrsRef = document.getElementById('numbers');
+
+
 function createNumbersArray(){
-  for(let i = 0; i < 65; i++){
-    allNumbers.push(i);
+  for(let i = 1; i < 51 ; i++){
+    if(i < 11){
+      allNumbers.push('B ' + i);
+    } else if(i > 10 && i < 21){
+      allNumbers.push('I ' + i);
+    } else if(i > 20 && i < 31){
+      allNumbers.push('N ' + i);
+    } else if(i > 30 && i < 41){
+      allNumbers.push('G ' + i);
+    } else if(i > 40 && i < 51){
+      allNumbers.push('O ' + i);
+    }
   }
+
 }
 
 createNumbersArray();
 
-document.getElementsByClassName('btn')[0].addEventListener('click', () => {
+document.body.addEventListener('click', () => {
   var item = allNumbers[Math.floor(Math.random() * allNumbers.length)]
-
-  
-  document.getElementById('number').innerHTML = item;
-
   allNumbers.splice(allNumbers.indexOf(item),1);
 
-  document.getElementById('numbers').innerHTML = document.getElementById('numbers').innerHTML + item + ', ';
+  let loop = 0;
+
+  if(allNumbers.length > 0){
+    chosenNrRef.style.color = '#b2bec3';
+
+    var myVar = setInterval(myTimer, 120);
+  }
+
+  function myTimer() {
+    chosenNrRef.innerHTML = allNumbers[Math.floor(Math.random() * allNumbers.length)];
+    
+    loop ++;
+
+    if(loop === 10){
+      chosenNrRef.innerHTML = item;
+      chosenNrRef.style.color = '#ffeaa7';
+
+      let newNrSpan = document.createElement('span');
+      newNrSpan.innerHTML = item.replace(/ /g,'');
+      newNrSpan.className = 'nr-span';
+
+      drawnNrsRef.appendChild(newNrSpan);
+      newNrSpan.addEventListener('click', (e)=>{
+        e.stopPropagation();
+        e.target.style.backgroundColor = '#0984e3';
+      })
+      // drawnNrsRef.innerHTML = drawnNrsRef.innerHTML + item.replace(/ /g,'') + ', ';
+
+      drawn ++;
+      document.getElementById('drawn').innerHTML = drawn;
+
+      myStopFunction();
+    }
+  }
+
+  function myStopFunction() {
+    clearInterval(myVar);
+  }
 })
 
